2e0f2ae424bb1ec330eaf017002ffae3e42de86b
Ekaterina Kovaleva <katekote4@gmail.com>
2023-04-04 00:38:05 +0300

    Lab2

--- Тема 3/Лабораторные работы/task8/main.py
+++ Тема 3/Лабораторные работы/task8/main.py
@@ -2,9 +2,12 @@ money_capital = 10000
 salary = 5000
 spend = 6000
 increase = 0.05
-
-month = 0  # количество месяцев, которое можно прожить
-
+sum1=salary+money_capital
+month=0
+while sum1>0:
+ sum1-=spend
+ spend*=1.05
+ month+=1  # количество месяцев, которое можно прожить
 # TODO Оформить решение
 
 print(month)
--- Тема 3/Лабораторные работы/task9/main.py
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -1,10 +1,12 @@
 salary = 5000  # зарплата
 spend = 6000  # траты
 months = 10  # количество месяцев
-increase = 0.03  # рост цен
-
-need_money = 0  # количество денег, чтобы прожить 10 месяцев
-
+increase = 1.03  # рост цен
+need_money = 0
+for i in range(months):
+    diff = spend-salary
+    need_money += diff
+    spend *= increase
+#need_money=0 # количество денег, чтобы прожить 10 месяцев
 # TODO Оформить решение
-
 print(round(need_money))
